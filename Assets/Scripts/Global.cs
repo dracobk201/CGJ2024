042a2669884245208742f2187344a87b80ffea41
Erick Subero <suberoerick@gmail.com>
2024-01-27 15:18:24 -0400

    Pedestrian Animator
    - Added corp elements to build.
    - Added Pedestrian Animator Config.

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -1,6 +1,15 @@
 public class Global
 {
+    #region Tags
     public const string ClownTag = "Clown";
     public const string PedestrianTag = "Pedestrian";
     public const string BulletTag = "ClownBullet";
+    #endregion
+
+    #region Animations
+    public const string FirstStatePedestrian = "First State";
+    public const string SecondStatePedestrian = "Second State";
+    public const string ThirdStatePedestrian = "Third State";
+    public const string ExplodePedestrian = "Explode State";
+    #endregion
 }
--- Assets/Scripts/Pedestrian/PedestrianFunness.cs
+++ Assets/Scripts/Pedestrian/PedestrianFunness.cs
@@ -7,6 +7,8 @@ public class PedestrianFunness : MonoBehaviour
     [SerializeField] private IntReference remainingPedestrian;
     [SerializeField] private IntReference funnessThreshold;
     [SerializeField] private SpriteRenderer pedestrianSpriteRenderer;
+    [SerializeField] private Animator pedestrianAnimator;
+    [SerializeField] private BoxCollider2D pedestrianCollider;
     private bool pedestrianHappy;
     private int currentFunness;
 
@@ -14,6 +16,7 @@ public class PedestrianFunness : MonoBehaviour
     {
         currentFunness = funnessThreshold.Value;
         pedestrianHappy = false;
+        pedestrianCollider.enabled = true;
     }
 
     private void Update()
@@ -30,9 +33,37 @@ public class PedestrianFunness : MonoBehaviour
         if (targetTag.Equals(Global.BulletTag) && !pedestrianHappy)
         {
             currentFunness--;
-            if (currentFunness <= 0)
+            float currentPercentage = currentFunness / funnessThreshold.Value;
+
+            if (currentPercentage > 0.8f)
+            {
+                if (pedestrianAnimator == null) return;
+                pedestrianAnimator.SetBool(Global.FirstStatePedestrian, true);
+                pedestrianAnimator.SetBool(Global.SecondStatePedestrian, false);
+                pedestrianAnimator.SetBool(Global.ThirdStatePedestrian, false);
+            }
+            else if (currentPercentage <= 0.8f && currentPercentage >= 0.4f)
+            {
+                if (pedestrianAnimator == null) return;
+                pedestrianAnimator.SetBool(Global.FirstStatePedestrian, false);
+                pedestrianAnimator.SetBool(Global.SecondStatePedestrian, true);
+                pedestrianAnimator.SetBool(Global.ThirdStatePedestrian, false);
+            }
+            else if (currentPercentage <= 0.4f && currentPercentage >= 0f)
+            {
+                if (pedestrianAnimator == null) return;
+                pedestrianAnimator.SetBool(Global.FirstStatePedestrian, false);
+                pedestrianAnimator.SetBool(Global.SecondStatePedestrian, false);
+                pedestrianAnimator.SetBool(Global.ThirdStatePedestrian, true);
+            }
+            else if (currentPercentage <= 0)
             {
                 pedestrianHappy = true;
+                if (pedestrianAnimator != null)
+                {
+                    pedestrianAnimator.SetBool(Global.ExplodePedestrian, true);
+                }
+                pedestrianCollider.enabled = false;
                 remainingPedestrian.Value = Mathf.Clamp(remainingPedestrian.Value - 1, 0, 9999);
             }
         }
